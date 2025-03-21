06f836052b4da28a5be55280f2b5a493ccbb8758
Kaopong Sangkittiyakorn <kuikaopong.2002@gmail.com>
2025-03-22 04:58:13 +0700

    update to activate venv

--- run_scripts/run_check_commission_daily.ps1
+++ run_scripts/run_check_commission_daily.ps1
@@ -1,3 +1,7 @@
+cd C:\Users\kuisskui\Desktop\discord-notification
+
+.\venv\Scripts\activate
+
 cd C:\Users\kuisskui\Desktop\discord-notification\run_scripts
 
 python .\check_commission_daily.py
--- run_scripts/run_rsi_4h.ps1
+++ run_scripts/run_rsi_4h.ps1
@@ -1,3 +1,7 @@
+cd C:\Users\kuisskui\Desktop\discord-notification
+
+.\venv\Scripts\activate
+
 cd C:\Users\kuisskui\Desktop\discord-notification\run_scripts
 
 python .\run_rsi_4h.py
